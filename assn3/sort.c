07d4f648e310dbbd5eaa056626e52389593a1f2b
Hai <yanhai12345@gmail.com>
2015-09-23 16:07:24 -0500

    a little more change

--- assn3/sort.c
+++ assn3/sort.c
@@ -13,7 +13,7 @@ void sort(int* A, int n){
 		j=i-1;
 		//For each element in A, search for where it belong in the subarray preceeding it's current location
 		while(tmp<A[j] && j>=0){
-			A[j+1]=A[j];
+			*(A+j+1)=A[j];
 			j-=1;
 		}
 		A[j+1]=tmp;
